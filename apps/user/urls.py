e75679605bc7b38f538e4abbf78b6dd9c3f27200
Huxifan <xifan_1995@163.com>
2019-11-05 22:55:56 +0800

    更新视图

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,7 +1,8 @@
 
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from apps.user import views
 urlpatterns = [
+    url(r'^register', views.register, name='register')  # 注册 # 使用反向解析添加name属性
 
 ]
