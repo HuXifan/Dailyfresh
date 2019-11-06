9d71f72fae8d2649f0db2eaf81f4b79b4d7f485a
Huxifan <xifan_1995@163.com>
2019-11-06 15:54:16 +0800

    使用类视图代替判断函数

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,9 +1,23 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from apps.user import views
-app_name = 'user'
+from user.views import RegisterView
+
 urlpatterns = [
-    url(r'^register$', views.register, name='register'),  # 注册 # 使用反向解析添加name属性
-    url(r'^register_handler$', views.register_handle, name='register_handler'),  # 注册处理
+    # url(r'^register$', views.register, name='register'),  # 注册 # 使用反向解析添加name属性
+    # url(r'^register_handler$', views.register_handle, name='register_handler'),  # 注册处理
+    url(r'^register$', RegisterView.as_view(), name='register')  # 注册
 
 ]
+
+
+'''
+def view(request, *args, **kwargs):
+    self = cls(**initkwargs)
+    if hasattr(self, 'get') and not hasattr(self, 'head'):
+        self.head = self.get
+    self.request = request
+    self.args = args
+    self.kwargs = kwargs
+    return self.dispatch(request, *args, **kwargs)  # 分发
+'''
