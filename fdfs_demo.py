3037b353c2ce61c50afdeebd4bc614f9b1893a5c
Huxifan <xifan_1995@163.com>
2019-11-16 16:25:40 +0800

    配置使用fdfs+nginx

---
+++ fdfs_demo.py
@@ -0,0 +1,12 @@
+from fdfs_client.client import Fdfs_client
+
+client = Fdfs_client('/etc/fdfs/client.conf')
+ret = client.upload_by_filename('fdfs_demo.py')
+print('__' * 10)
+print(ret)
+'''
+{'Local file name': 'fdfs_demo.py', 'Storage IP': '10.10.21.29', 'Remote file_id': 'group1/M00/00/00/CgoVHV3PsfCAZU9qAAAAtYX8Org2798.py', 'Status': 'Upload successed.', 'Uploaded size': '181B', 'Group name': 'group1'}
+
+'''
+print('__' * 10)
+
