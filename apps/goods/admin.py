a0bdd6748a9a7270ee3dd84f6573fd21ead3325f
Huxifan <xifan_1995@163.com>
2019-11-23 22:05:55 +0800

    修改Admin多字段显示

--- apps/goods/admin.py
+++ apps/goods/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.core.cache import cache
-from goods.models import GoodsType, GoodsSKU, IndexPromotionBanner, IndexGoodsBanner, IndexTypeGoodsBanner
+from goods.models import Goods, GoodsType, GoodsSKU, IndexPromotionBanner, IndexGoodsBanner, IndexTypeGoodsBanner
 
 
 # Register your models here.
@@ -26,27 +26,80 @@ class BaseModelAdmin(admin.ModelAdmin):
         cache.delete('index_page_data')
 
 
+# 商品SPU模型类
+class GoodsAdmin(BaseModelAdmin):
+    # 需要显示的字段信息
+    list_display = ('id', 'name', 'detail')
+    # 设置哪些字段可以点击进入编辑界面，默认是第一个字段
+    list_display_links = ('id', 'name')
+    '''每页显示条目数'''
+    list_per_page = 50
+    pass
+
+
+# 商品类型
 class GoodsTypeAdmin(BaseModelAdmin):
-    pass
+    # 需要显示的字段信息
+    list_display = ('id', 'name', 'logo')
+    # 设置哪些字段可以点击进入编辑界面，默认是第一个字段
+    list_display_links = ('id', 'name')
+    '''每页显示条目数'''
+    list_per_page = 20
 
 
+# 商品SKU模型类
 class GoodsSKUAdmin(BaseModelAdmin):
-    pass
+    # 需要显示的字段信息
+    list_display = ('id', 'type', 'name', 'status')
+    # 设置哪些字段可以点击进入编辑界面，默认是第一个字段
+    list_display_links = ('id', 'name')
+    '''每页显示条目数'''
+    list_per_page = 20
 
 
+# 首页轮播商品
 class IndexGoodsBannerAdmin(BaseModelAdmin):
+    # 需要显示的字段信息
+    list_display = ('id', 'sku', 'sku_name', 'index')
+    # 设置哪些字段可以点击进入编辑界面，默认是第一个字段
+    list_display_links = ('id', 'sku')
+    '''每页显示条目数'''
+    list_per_page = 10
+
+    def sku_name(self, obj):
+        return '%s' % obj.sku.name  # ☆
+
+    sku_name.short_description = '商品名'
     pass
 
 
+# 主页分类展示商品
 class IndexTypeGoodsBannerAdmin(BaseModelAdmin):
-    pass
+    # 需要显示的字段信息
+    list_display = ('id', 'sku_name', 'type', 'display_type')
+    # 设置哪些字段可以点击进入编辑界面，默认是第一个字段
+    list_display_links = ('id', 'sku_name')
+    '''每页显示条目数'''
+    list_per_page = 20
+
+    def sku_name(self, obj):
+        return '%s' % obj.sku.name  # ☆
+
+    sku_name.short_description = '商品名'
 
 
+# 首页促销活动模型类
 class IndexPromotionBannerAdmin(BaseModelAdmin):
-    pass
+    # 需要显示的字段信息
+    list_display = ('id', 'name', 'index')
+    # 设置哪些字段可以点击进入编辑界面，默认是第一个字段
+    list_display_links = ('id', 'name')
+    '''每页显示条目数'''
+    list_per_page = 20
 
 
 admin.site.register(GoodsType, GoodsTypeAdmin)
+admin.site.register(Goods, GoodsAdmin)
 admin.site.register(GoodsSKU, GoodsSKUAdmin)
 admin.site.register(IndexGoodsBanner, IndexGoodsBannerAdmin)
 admin.site.register(IndexTypeGoodsBanner, IndexTypeGoodsBannerAdmin)
