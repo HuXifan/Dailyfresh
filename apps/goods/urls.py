112a200e0f417f80b2d355e70de632539a969dfa
Huxifan <xifan_1995@163.com>
2019-11-25 18:21:37 +0800

    配置列表页url

--- apps/goods/urls.py
+++ apps/goods/urls.py
@@ -1,8 +1,10 @@
-from goods.views import IndexView, DetailView
+from goods.views import IndexView, DetailView, ListView
 from django.conf.urls import include, url
 from django.contrib import admin
 from goods import views
+
 urlpatterns = [
-    url(r'^index$', IndexView.as_view(), name='index') , # 首页
-    url(r'^goods/(?P<goods_id>\d+)$', DetailView.as_view(), name='detail')  # 详情
+    url(r'^index$', IndexView.as_view(), name='index'),  # 首页
+    url(r'^goods/(?P<goods_id>\d+)$', DetailView.as_view(), name='detail'),  # 详情
+    url(r'^list/(?P<type_id>\d+)/(?P<page>\d+)$', ListView.as_view(), name='list'),  # /list/种类id/页码?sort=排序方式  列表页
 ]
