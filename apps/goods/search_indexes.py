0558ac4b1802323695812f5dd4b26485e041a988
Huxifan <xifan_1995@163.com>
2019-11-27 22:34:18 +0800

    update code

--- apps/goods/search_indexes.py
+++ apps/goods/search_indexes.py
@@ -6,11 +6,11 @@ from goods.models import GoodsSKU
 
 # 指定对于某个类的某些数据建立索引 模型类名+Index
 class GoodsSKUIndex(indexes.SearchIndex, indexes.Indexable):
-    # 索引字段，use_template=True． 指定表中哪些字段建立索引数据，把说明放在一个文件中
+    # 索引字段，use_template=True 指定表中哪些字段建立索引数据，把说明放在一个文件（templates/search/indexes/goods里面）中
     text = indexes.CharField(document=True, use_template=True)
 
     def get_model(self):
-        return GoodsSKU
+        return GoodsSKU  # 返回模型类
 
     # 建立索引数据
     def index_queryset(self, using=None):
