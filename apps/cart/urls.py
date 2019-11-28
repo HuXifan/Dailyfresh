27945af78cce246b4e4638cada569264301c9c22
Huxifan <xifan_1995@163.com>
2019-11-28 19:54:11 +0800

    购物车后台记录添加

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -1,7 +1,6 @@
-
-from django.conf.urls import  url
-
+from django.conf.urls import url
+from cart.views import CartAddView
 
 urlpatterns = [
-
+    url(r'^add$', CartAddView.as_view(), name='add'),  # 够驱车记录添加
 ]
