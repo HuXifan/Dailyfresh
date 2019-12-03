fc52fd2fda3400ffb6567abcec4d93407963b511
Huxifan <xifan_1995@163.com>
2019-12-03 17:06:45 +0800

    订单后台view

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from order.views import OrderPlaceView
+from order.views import OrderPlaceView, OrderCommitView
 
 urlpatterns = [
     url(r'^place$', OrderPlaceView.as_view(), name='place'),  # 提交订单页面显示
+    url(r'^commit$', OrderCommitView.as_view(), name='commit'),  # 订单创建
 ]
